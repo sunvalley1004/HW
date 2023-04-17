0d028e350d91a475d54755ac0e13aed82dfe54ea
sunvalley1004 <sunvalley1004@gmail.com>
2023-04-17 19:32:07 +0900

    first upload

---
+++ public/index.14167ddb.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button"),t=document.querySelector("body");let o=["darkslateblue","midnightblue","teal","tomato","seagreen","royalblue","saddlebrown","indigo","olivedrab","rosybrown"];e.addEventListener("click",(function(){const e=Math.floor(Math.random()*o.length);t.style.setProperty("--bg",o[e])}));
+//# sourceMappingURL=index.14167ddb.js.map
